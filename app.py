a6cfcedc8af77bc7c6e08717f67ee481f963cac7
Hadsapong Lee <135619627+HaG40@users.noreply.github.com>
2025-04-30 09:56:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ heart_data = pd.read_csv('dataset.csv')
 heart_data.head()
 X = heart_data.drop(columns='target', axis=1)
 Y = heart_data['target']
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, stratify=Y, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=42)
 
 # Logistic Regression Model
 # model = LogisticRegression()
